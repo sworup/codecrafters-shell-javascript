445a87a3c3bfe1a5113596dc02b535038d80579c
Sworup Shakya <sworup@users.noreply.github.com>
2025-11-21 17:59:44 -0600

    Implement premitive exit

--- app/main.js
+++ app/main.js
@@ -7,6 +7,10 @@ const rl = readline.createInterface({
 
 function askQuestion() {
   rl.question("$ ", (answer) => {
+    if(answer === "exit") {
+      rl.close();
+      return;
+    }
     console.log(`${answer}: command not found`);
     askQuestion();
   });
