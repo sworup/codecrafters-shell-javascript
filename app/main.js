23c771ffc3ea002532f9a273bfafad952e762a15
Sworup Shakya <sworup@users.noreply.github.com>
2025-11-22 17:57:44 -0600

    Implement path interpolation

--- app/main.js
+++ app/main.js
@@ -1,4 +1,5 @@
 const readline = require("readline");
+const path = require("path");
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -16,6 +17,20 @@ const commands = {
         return;
       }
     }
+
+    const pathVar = process.env.PATH || "";
+    const paths = pathVar.split(path.delimiter);
+    for (const p of paths) {
+      const fullPath = path.join(p, args[0]);
+      try {
+        require("fs").accessSync(fullPath);
+        console.log(`${args[0]} is ${fullPath}`);
+        return;
+      } catch (err) {
+      }
+    }
+
+
     console.log(`${args[0]}: not found`);
   },
   exit: () => {
