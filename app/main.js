958b2cca89d1b1840ea65c7df337d1ef1b3d76dd
Sworup Shakya <sworup@users.noreply.github.com>
2025-11-23 16:13:21 -0600

    Implement path interpolation

--- app/main.js
+++ app/main.js
@@ -1,4 +1,6 @@
 import {rl, commands } from "./commands.js";
+import { execFile } from "child_process";   
+
 
 function askQuestion() {
   rl.question("$ ",  (answer) => {
@@ -11,8 +13,15 @@ function askQuestion() {
       }
     }
     
-    console.log(`${commandArr[0]}: command not found`);
-     askQuestion();
+    execFile(commandArr[0], commandArr.slice(1), (error, stdout, stderr) => {
+      if (error) {
+        console.error(error.message);
+      } else {
+        process.stdout.write(stdout);
+        process.stderr.write(stderr);
+      }
+      askQuestion();
+    });
   });
 }
 
